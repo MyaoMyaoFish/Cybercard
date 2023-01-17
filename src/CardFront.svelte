41ff012963356fbf2056225b0122bd7a6403b826
myaomyaofish <qf.zheng@outlook.com>
2023-01-17 22:13:50 +0800

    更新自己的信息

--- src/CardFront.svelte
+++ src/CardFront.svelte
@@ -1,13 +1,14 @@
 <div class='h-[200px] w-full flex'>
   <div class='flex-1 flex flex-col justify-between p-4'>
     <span class='text-4xl font-bold'>嘉何</span>
-    {'17 岁，'}
+    {'♂，'}
     <br>
-    {'在读高三。'}
+    {'硕士在读。'}
     <br>
-    {'广州出没，'}
+    {'南宁出没，'}
     <br>
-    {'十八线水平后期。'}
+    {'吟诗一首：'<span id="jinrishici-sentence">正在加载今日诗词....</span>
+<script src="https://sdk.jinrishici.com/v2/browser/jinrishici.js" charset="utf-8"></script>}
   </div>
   <img alt='' class='rounded-r-lg' src='/image/avatar.png' />
 </div>
